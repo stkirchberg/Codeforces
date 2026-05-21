e6c35a77d4e5791be93b3478efe4259ff3c9b263
stkirchberg <s.t.kirchberg@web.de>
2026-05-21 18:30:46 +0200

    F correct

--- 1099 /E. Graph Cutting/main.go
+++ 1099 /E. Graph Cutting/main.go
@@ -1,91 +1,126 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
-var reader = bufio.NewReader(os.Stdin)
-
-func solve() {
-	var n, d int
-	fmt.Fscan(reader, &n, &d)
-
-	adj := make([][]int, n+1)
-	for i := 0; i < n-1; i++ {
-		var u, v int
-		fmt.Fscan(reader, &u, &v)
-		adj[u] = append(adj[u], v)
-		adj[v] = append(adj[v], u)
+func readInt(b []byte, idx *int) int {
+	for *idx < len(b) && (b[*idx] < '0' || b[*idx] > '9') {
+		*idx++
 	}
-
-	ans := int64(0)
-
-	for m := 1; m <= n; m++ {
-		var branches [][]int
-		for _, neighbor := range adj[m] {
-			distCounts := make([]int, n+1)
-			var dfs func(u, p, depth int)
-			dfs = func(u, p, depth int) {
-				distCounts[depth]++
-				for _, v := range adj[u] {
-					if v != p {
-						dfs(v, u, depth+1)
-					}
-				}
-			}
-			dfs(neighbor, m, 1)
-			branches = append(branches, distCounts)
-		}
-
-		pairs := make([]int64, n+1)
-		singles := make([]int64, n+1)
-
-		for _, b := range branches {
-			for s1 := 1; s1 <= n; s1++ {
-				if pairs[s1] == 0 {
-					continue
-				}
-				for s2 := 1; s2 <= n; s2++ {
-					if b[s2] == 0 {
-						continue
-					}
-					if s1+s2+1 == d {
-						ans += pairs[s1] * int64(b[s2])
-					}
-				}
-			}
-
-			for s1 := 1; s1 <= n; s1++ {
-				if b[s1] == 0 {
-					continue
-				}
-				for s2 := 1; s2 <= n; s2++ {
-					if singles[s2] == 0 {
-						continue
-					}
-					pairs[s1+s2] += singles[s2] * int64(b[s1])
-				}
-			}
-
-			for s := 1; s <= n; s++ {
-				singles[s] += int64(b[s])
-			}
-		}
-
-		if d == 1 {
-			continue
-		}
+	if *idx >= len(b) {
+		return 0
 	}
-
-	fmt.Println(ans)
+	res := 0
+	for *idx < len(b) && b[*idx] >= '0' && b[*idx] <= '9' {
+		res = res*10 + int(b[*idx]-'0')
+		*idx++
+	}
+	return res
 }
 
 func main() {
-	var t int
-	fmt.Fscan(reader, &t)
-	for i := 0; i < t; i++ {
-		solve()
+	b, _ := io.ReadAll(os.Stdin)
+	idx := 0
+	if idx >= len(b) {
+		return
 	}
+	t := readInt(b, &idx)
+	for i := 0; i < t; i++ {
+		n := readInt(b, &idx)
+		d := readInt(b, &idx)
+		adj := make([][]int, n+1)
+		for j := 0; j < n-1; j++ {
+			u := readInt(b, &idx)
+			v := readInt(b, &idx)
+			adj[u] = append(adj[u], v)
+			adj[v] = append(adj[v], u)
+		}
+		solve(n, d, adj)
+	}
+}
+
+func solve(n, d int, adj [][]int) {
+	var ans int64 = 0
+
+	var dfs func(u, p int) ([]int64, []int64)
+	dfs = func(u, p int) ([]int64, []int64) {
+		fu := make([]int64, 1)
+		fu[0] = 1
+		gu := make([]int64, 0)
+
+		for _, v := range adj[u] {
+			if v == p {
+				continue
+			}
+			fv, gv := dfs(v, u)
+
+			for i := 0; i < len(gv); i++ {
+				j := d - 2 - i
+				if j >= 0 && j < len(fu) {
+					ans += gv[i] * fu[j]
+				}
+			}
+			for i := 0; i < len(gu); i++ {
+				j := d - 2 - i
+				if j >= 0 && j < len(fv) {
+					ans += gu[i] * fv[j]
+				}
+			}
+
+			newLenF := len(fu)
+			if len(fv)+1 > newLenF {
+				newLenF = len(fv) + 1
+			}
+			if newLenF > d {
+				newLenF = d
+			}
+
+			newLenG := len(gu)
+			if len(gv)+1 > newLenG {
+				newLenG = len(gv) + 1
+			}
+			if len(fu)+len(fv) > newLenG {
+				newLenG = len(fu) + len(fv)
+			}
+			if newLenG > d {
+				newLenG = d
+			}
+
+			newF := make([]int64, newLenF)
+			newG := make([]int64, newLenG)
+
+			copy(newF, fu)
+			copy(newG, gu)
+
+			for i := 0; i < len(fu); i++ {
+				for j := 0; j < len(fv); j++ {
+					if i+j+1 < newLenG {
+						newG[i+j+1] += fu[i] * fv[j]
+					}
+				}
+			}
+
+			for i := 0; i < len(gv); i++ {
+				if i+1 < newLenG {
+					newG[i+1] += gv[i]
+				}
+			}
+
+			for j := 0; j < len(fv); j++ {
+				if j+1 < newLenF {
+					newF[j+1] += fv[j]
+				}
+			}
+
+			fu = newF
+			gu = newG
+		}
+		return fu, gu
+	}
+
+	dfs(1, 0)
+	fmt.Println(ans)
 }
