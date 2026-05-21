b0979e29dd722687b6e22abbd15fd7cb26b5c19d
stkirchberg <s.t.kirchberg@web.de>
2026-05-21 17:32:20 +0200

    C correct

--- 1099 /C. Chipmunk Theo and Equality/main.go
+++ 1099 /C. Chipmunk Theo and Equality/main.go
@@ -3,52 +3,88 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 )
 
-func getOps(val int, target int) int64 {
-	ops := int64(0)
-	curr := val
-	for curr > target {
-		if curr%2 == 0 {
-			curr /= 2
-		} else {
-			curr++
+func solve(in *bufio.Reader, out *bufio.Writer) {
+	var t int
+	fmt.Fscan(in, &t)
+
+	for tc := 0; tc < t; tc++ {
+		var n int
+		fmt.Fscan(in, &n)
+
+		a := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &a[i])
 		}
-		ops++
+
+		candidates := make([]int, 0, 105)
+		candidateIdx := make(map[int]int)
+
+		curr := a[0]
+		for steps := 0; steps <= 100; steps++ {
+			if _, exists := candidateIdx[curr]; !exists {
+				candidateIdx[curr] = len(candidates)
+				candidates = append(candidates, curr)
+			}
+
+			if curr%2 == 0 {
+				curr /= 2
+			} else {
+				curr += 1
+			}
+		}
+
+		numCands := len(candidates)
+		totalCost := make([]int, numCands)
+		validCount := make([]int, numCands)
+		localDist := make([]int, numCands)
+
+		for _, val := range a {
+			for i := 0; i < numCands; i++ {
+				localDist[i] = -1
+			}
+
+			curr := val
+			for steps := 0; steps <= 100; steps++ {
+				if idx, exists := candidateIdx[curr]; exists {
+					if localDist[idx] == -1 {
+						localDist[idx] = steps
+					}
+				}
+				if curr%2 == 0 {
+					curr /= 2
+				} else {
+					curr += 1
+				}
+			}
+
+			for i := 0; i < numCands; i++ {
+				if localDist[i] != -1 {
+					totalCost[i] += localDist[i]
+					validCount[i]++
+				}
+			}
+		}
+
+		ans := -1
+		for i := 0; i < numCands; i++ {
+			if validCount[i] == n {
+				if ans == -1 || totalCost[i] < ans {
+					ans = totalCost[i]
+				}
+			}
+		}
+
+		fmt.Fprintln(out, ans)
 	}
-	if curr == target {
-		return ops
-	}
-	return 1e15
 }
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	var t int
-	fmt.Fscan(reader, &t)
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
 
-	for i := 0; i < t; i++ {
-		var n int
-		fmt.Fscan(reader, &n)
-		a := make([]int, n)
-		for j := 0; j < n; j++ {
-			fmt.Fscan(reader, &a[j])
-		}
-
-		minTotalOps := int64(math.MaxInt64)
-
-		for k := 0; k <= 30; k++ {
-			target := 1 << k
-			currentTotalOps := int64(0)
-			for _, val := range a {
-				currentTotalOps += getOps(val, target)
-			}
-			if currentTotalOps < minTotalOps {
-				minTotalOps = currentTotalOps
-			}
-		}
-		fmt.Println(minTotalOps)
-	}
+	solve(in, out)
 }
