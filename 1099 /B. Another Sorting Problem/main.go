63858b26aef1401059ea61e0ef03abdf724e2a2f
stkirchberg <s.t.kirchberg@web.de>
2026-05-21 16:56:18 +0200

    .

--- 1099 /B. Another Sorting Problem/main.go
+++ 1099 /B. Another Sorting Problem/main.go
@@ -6,17 +6,11 @@ import (
 	"os"
 )
 
-func isSorted(a []int) bool {
-	for i := 0; i < len(a)-1; i++ {
-		if a[i] > a[i+1] {
-			return false
-		}
-	}
-	return true
-}
-
 func solve() {
 	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
 	var t int
 	fmt.Fscan(reader, &t)
 
@@ -28,32 +22,42 @@ func solve() {
 			fmt.Fscan(reader, &a[i])
 		}
 
-		if isSorted(a) {
-			fmt.Println("YES")
-			continue
-		}
-
-		l := 0
+		idx := -1
 		for i := 0; i < n-1; i++ {
 			if a[i] > a[i+1] {
-				l = i
+				idx = i
 				break
 			}
 		}
 
-		k := a[l] - a[l+1] + 1
-
-		b := make([]int, n)
-		copy(b, a)
-
-		for i := l + 1; i < n; i++ {
-			b[i] += k
+		if idx == -1 {
+			fmt.Fprintln(writer, "YES")
+			continue
 		}
 
-		if isSorted(b) {
-			fmt.Println("YES")
+		k := a[idx] - a[idx+1]
+
+		possible := true
+		prev := -1
+
+		for i := 0; i < n; i++ {
+			val := a[i]
+
+			if i > idx {
+				val += k
+			}
+
+			if val < prev {
+				possible = false
+				break
+			}
+			prev = val
+		}
+
+		if possible {
+			fmt.Fprintln(writer, "YES")
 		} else {
-			fmt.Println("NO")
+			fmt.Fprintln(writer, "NO")
 		}
 	}
 }
