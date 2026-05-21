4eaf4921fdbb72b4a735735211fabac1341fe7c9
stkirchberg <s.t.kirchberg@web.de>
2026-05-21 14:03:47 +0200

    prison break

---
+++ 1482/Prison Break/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var t int
+
+	_, err := fmt.Scan(&t)
+	if err != nil {
+		return
+	}
+
+	for i := 0; i < t; i++ {
+		var a, b int
+
+		_, err := fmt.Scan(&a, &b)
+		if err != nil {
+			break
+		}
+
+		fmt.Println(a * b)
+	}
+}
