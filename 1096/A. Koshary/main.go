459c72bc8393564dd03bbe5093abb3e4c93da9fd
stkirchberg <s.t.kirchberg@web.de>
2026-05-22 12:46:58 +0200

    A correct

--- 1096/A. Koshary/main.go
+++ 1096/A. Koshary/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var t int
+	fmt.Scan(&t)
+
+	for i := 0; i < t; i++ {
+		var x, y int
+		fmt.Scan(&x, &y)
+
+		if x%2 != 0 && y%2 != 0 {
+			fmt.Println("NO")
+		} else {
+			fmt.Println("YES")
+		}
+	}
+}
