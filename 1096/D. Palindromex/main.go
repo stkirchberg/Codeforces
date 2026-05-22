8daad140f1a6f562d9d8aafcba5701be5e5d50c9
stkirchberg <s.t.kirchberg@web.de>
2026-05-22 13:06:58 +0200

    D wrong

--- 1096/D. Palindromex/main.go
+++ 1096/D. Palindromex/main.go
@@ -2,98 +2,84 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
 const (
 	base = 100003
-	mod1 = 1000000007
-	mod2 = 1000000009
+	mod  = 1000000007
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Split(bufio.ScanWords)
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
 
-	next := func() int {
-		scanner.Scan()
-		val, _ := strconv.Atoi(scanner.Text())
-		return val
-	}
+	var t int
+	_, _ = fmt.Fscan(in, &t)
 
-	pow1 := make([]int64, 200005)
-	pow2 := make([]int64, 200005)
-	pow1[0], pow2[0] = 1, 1
-	for i := 1; i < 200005; i++ {
-		pow1[i] = (pow1[i-1] * base) % mod1
-		pow2[i] = (pow2[i-1] * base) % mod2
-	}
-
-	t := next()
 	for i := 0; i < t; i++ {
-		n := next()
+		var n int
+		_, _ = fmt.Fscan(in, &n)
 		size := 2 * n
 		a := make([]int, size)
-		pos := make([][2]int, n)
-		seen := make([]bool, n)
+
+		pos := make([][2]int, n+1)
+		for j := 0; j <= n; j++ {
+			pos[j][0] = size + 1
+			pos[j][1] = -1
+		}
+
 		for j := 0; j < size; j++ {
-			a[j] = next()
-			if !seen[a[j]] {
+			fmt.Fscan(in, &a[j])
+			if j < pos[a[j]][0] {
 				pos[a[j]][0] = j
-				seen[a[j]] = true
-			} else {
+			}
+			if j > pos[a[j]][1] {
 				pos[a[j]][1] = j
 			}
 		}
 
-		h1f := make([]int64, size+1)
-		h2f := make([]int64, size+1)
-		h1b := make([]int64, size+1)
-		h2b := make([]int64, size+1)
+		pow := make([]int64, size+1)
+		pow[0] = 1
+		for j := 1; j <= size; j++ {
+			pow[j] = (pow[j-1] * base) % mod
+		}
 
+		h1 := make([]int64, size+1)
+		h2 := make([]int64, size+1)
 		for j := 0; j < size; j++ {
-			h1f[j+1] = (h1f[j]*base + int64(a[j])) % mod1
-			h2f[j+1] = (h2f[j]*base + int64(a[j])) % mod2
-		}
-		for j := size - 1; j >= 0; j-- {
-			h1b[j] = (h1b[j+1]*base + int64(a[j])) % mod1
-			h2b[j] = (h2b[j+1]*base + int64(a[j])) % mod2
+			h1[j+1] = (h1[j]*base + int64(a[j]+1)) % mod
+			h2[j+1] = (h2[j]*base + int64(a[size-1-j]+1)) % mod
 		}
 
-		check := func(l, r int) bool {
+		isPal := func(l, r int) bool {
 			len := r - l + 1
-			f1 := (h1f[r+1] - h1f[l]*pow1[len]%mod1 + mod1) % mod1
-			f2 := (h2f[r+1] - h2f[l]*pow2[len]%mod2 + mod2) % mod2
-			b1 := (h1b[l] - h1b[r+1]*pow1[len]%mod1 + mod1) % mod1
-			b2 := (h2b[l] - h2b[r+1]*pow2[len]%mod2 + mod2) % mod2
-			return f1 == b1 && f2 == b2
+			val1 := (h1[r+1] - h1[l]*pow[len]%mod + mod) % mod
+			val2 := (h2[size-l] - h2[size-r-1]*pow[len]%mod + mod) % mod
+			return val1 == val2
 		}
 
 		minL, maxR := size, -1
 		ans := 0
-		for k := n; k >= 1; k-- {
-			val := k - 1
-			if pos[val][0] < minL {
-				minL = pos[val][0]
+
+		for k := 0; k < n; k++ {
+			if pos[k][0] < minL {
+				minL = pos[k][0]
 			}
-			if pos[val][1] < minL {
-				minL = pos[val][1]
-			}
-			if pos[val][0] > maxR {
-				maxR = pos[val][0]
-			}
-			if pos[val][1] > maxR {
-				maxR = pos[val][1]
+			if pos[k][1] > maxR {
+				maxR = pos[k][1]
 			}
 
-			if check(minL, maxR) {
-				ans = k
+			if minL <= maxR && isPal(minL, maxR) {
+				ans = k + 1
+			} else {
 				break
 			}
 		}
-		writer.WriteString(strconv.Itoa(ans) + "\n")
+
+		out.WriteString(strconv.Itoa(ans) + "\n")
 	}
 }
