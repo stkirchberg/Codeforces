8dc7b40e3f0fbd02f33e9d9857c1e7cf7df3601d
stkirchberg <s.t.kirchberg@web.de>
2026-05-23 18:28:34 +0200

    B correct

---
+++ 1100/B. Absolute Cinema/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+
+	for i := 0; i < t; i++ {
+		var n int
+		fmt.Fscan(in, &n)
+
+		a := make([]int64, n)
+		for j := 0; j < n; j++ {
+			fmt.Fscan(in, &a[j])
+		}
+
+		var sumMax int64 = 0
+		var maxMin int64 = 0
+
+		for j := 0; j < n; j++ {
+			var b int64
+			fmt.Fscan(in, &b)
+
+			maxVal := a[j]
+			minVal := b
+			if b > a[j] {
+				maxVal = b
+				minVal = a[j]
+			}
+
+			sumMax += maxVal
+			if minVal > maxMin {
+				maxMin = minVal
+			}
+		}
+
+		fmt.Fprintln(out, sumMax+maxMin)
+	}
+}
