4dec354f7c8548f5821b0dc886ffc1d1d105de50
stkirchberg <s.t.kirchberg@web.de>
2026-05-23 18:22:48 +0200

    A correct

---
+++ 1100/A. Slimes on a Line/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+
+	for i := 0; i < t; i++ {
+		var n int
+		fmt.Fscan(in, &n)
+
+		var min, max int
+		for j := 0; j < n; j++ {
+			var a int
+			fmt.Fscan(in, &a)
+
+			if j == 0 {
+				min = a
+				max = a
+			} else {
+				if a < min {
+					min = a
+				}
+				if a > max {
+					max = a
+				}
+			}
+		}
+
+		ans := (max - min + 1) / 2
+		fmt.Fprintln(out, ans)
+	}
+}
