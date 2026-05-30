3fdd71915b58662509d2db2f2995d085a3f33396
stkirchberg <s.t.kirchberg@web.de>
2026-05-30 16:45:12 +0200

    A solved

--- 1101/A. Convergence/main.go
+++ 1101/A. Convergence/main.go
@@ -1,1 +1,59 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func solve() {
+
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+
+	for tc := 0; tc < t; tc++ {
+		var n int
+		fmt.Fscan(reader, &n)
+
+		a := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(reader, &a[i])
+		}
+
+		minCalls := n
+
+		for i := 0; i < n; i++ {
+			target := a[i]
+			l, r := 0, 0
+
+			for j := 0; j < n; j++ {
+				if a[j] < target {
+					l++
+				} else if a[j] > target {
+					r++
+				}
+			}
+
+			calls := l
+			if r > l {
+				calls = r
+			}
+
+			if calls < minCalls {
+				minCalls = calls
+			}
+		}
+
+		fmt.Fprintln(writer, minCalls)
+	}
+}
+
+func main() {
+	solve()
+}
