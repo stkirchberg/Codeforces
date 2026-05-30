e0334a03c25239e87c55b40947426219a24f00da
stkirchberg <s.t.kirchberg@web.de>
2026-05-30 17:19:48 +0200

    C2 solved

--- 1101/C2. Seating Arrangement (Hard Version)/main.go
+++ 1101/C2. Seating Arrangement (Hard Version)/main.go
@@ -2,94 +2,69 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
+	"strconv"
 )
 
-func solve() {
-	reader := bufio.NewReader(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Buffer(make([]byte, 1024*1024), 10*1024*1024)
+	scanner.Split(bufio.ScanWords)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
 
-	var t int
-	if _, err := fmt.Fscan(reader, &t); err != nil {
+	if !scanner.Scan() {
 		return
 	}
+	t, _ := strconv.Atoi(scanner.Text())
 
 	for tc := 0; tc < t; tc++ {
-		var n, x, s int
-		fmt.Fscan(reader, &n, &x, &s)
+		scanner.Scan()
+		n, _ := strconv.Atoi(scanner.Text())
+		scanner.Scan()
+		x, _ := strconv.Atoi(scanner.Text())
+		scanner.Scan()
+		s, _ := strconv.Atoi(scanner.Text())
 
-		var u string
-		fmt.Fscan(reader, &u)
+		scanner.Scan()
+		u := scanner.Text()
 
-		count := make([]int, s+1)
-		size := 0
-
-		highPtr := s - 1
-		lowPtr := 0
-
-		addSlope := func(v int) {
-			count[v]++
-			size++
-			if v < lowPtr {
-				lowPtr = v
-			}
-			if v < s && v > highPtr {
-				highPtr = v
-			}
-			if size > x {
-				for lowPtr <= s && count[lowPtr] == 0 {
-					lowPtr++
-				}
-				if lowPtr <= s {
-					count[lowPtr]--
-					size--
-				}
-			}
-		}
+		X := x
+		S := 0
+		ans := 0
+		A_as_E := 0
 
 		for i := 0; i < n; i++ {
 			c := u[i]
 			if c == 'I' {
-				if count[0] > 0 || size < x {
-					addSlope(1)
+				if X > 0 {
+					X--
+					S += s - 1
+					ans++
 				}
 			} else if c == 'E' {
-				for highPtr >= 0 && count[highPtr] == 0 {
-					highPtr--
-				}
-				if highPtr >= 0 {
-					count[highPtr]--
-					count[highPtr+1]++
-					if highPtr+1 < s {
-						highPtr = highPtr + 1
-					}
+				if S > 0 {
+					S--
+					ans++
+				} else if A_as_E > 0 && X > 0 {
+					A_as_E--
+					X--
+					S += s
+					S--
+					ans++
 				}
 			} else if c == 'A' {
-				for highPtr >= 0 && count[highPtr] == 0 {
-					highPtr--
+				if S > 0 {
+					S--
+					A_as_E++
+					ans++
+				} else if X > 0 {
+					X--
+					S += s - 1
+					ans++
 				}
-				if highPtr >= 0 {
-					count[highPtr]--
-					count[highPtr+1]++
-					if highPtr+1 < s {
-						highPtr = highPtr + 1
-					}
-				}
-
-				addSlope(0)
 			}
 		}
-
-		var ans int64 = 0
-		for v := 1; v <= s; v++ {
-			ans += int64(v) * int64(count[v])
-		}
-		fmt.Fprintln(writer, ans)
+		out.WriteString(strconv.Itoa(ans) + "\n")
 	}
-}
-
-func main() {
-	solve()
 }
