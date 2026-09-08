f086d5e2ddea3695b47762f9db71b37a1d86112b
stkirchberg <s.t.kirchberg@web.de>
2026-09-08 17:58:49 +0200

    okay, new error: tiome limit

--- E194D.cpp
+++ E194D.cpp
@@ -1,49 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool f(int k, string& s, int n) {
-    long long l = 0, r = 0;
+struct P {
+    long long l, r;
+};
+
+string s;
+int n;
+
+bool chk(long long c) {
+    vector<P> a;
+    a.push_back({0, 0});
     for (int i = 0; i < n; i++) {
-        char c = s[i];
-        if (c == '0') {
-            bool w = (max(l, -(long long)k) <= min(r, -1LL)) || (max(l, 1LL) <= min(r, (long long)k));
-            if (!w) return 0;
-            l = 0; r = 0;
-        } else if (c == '+') {
-            long long u = r + k;
-            if (u < 1) return 0;
-            long long v = (r >= 2) ? max(1LL, l - k) : max(1LL, l + 1);
-            l = v; r = u;
-        } else {
-            long long v = l - k;
-            if (v > -1) return 0;
-            long long u = (l <= -2) ? min(-1LL, r + k) : min(-1LL, r - 1);
-            l = v; r = u;
+        vector<P> b;
+        for (auto x : a) {
+            if (x.l < x.r) {
+                b.push_back({x.l - c, x.r + c});
+            } else {
+                long long v = x.l;
+                b.push_back({v - c, v - 1});
+                b.push_back({v + 1, v + c});
+            }
+        }
+        vector<P> d;
+        char ch = s[i];
+        for (auto x : b) {
+            long long nl = x.l, nr = x.r;
+            if (ch == '+') {
+                nl = max(nl, 1LL);
+            } else if (ch == '-') {
+                nr = min(nr, -1LL);
+            } else {
+                nl = max(nl, 0LL);
+                nr = min(nr, 0LL);
+            }
+            if (nl <= nr) {
+                d.push_back({nl, nr});
+            }
+        }
+        if (d.empty()) return false;
+        
+        vector<P> m;
+        for (auto x : d) {
+            if (m.empty()) {
+                m.push_back(x);
+            } else {
+                if (x.l <= m.back().r + 1) {
+                    m.back().r = max(m.back().r, x.r);
+                } else {
+                    m.push_back(x);
+                }
+            }
+        }
+        a = m;
+    }
+    return !a.empty();
+}
+
+void run() {
+    cin >> n >> s;
+    if (s[0] == '0') {
+        cout << -1 << "\n";
+        return;
+    }
+    for (int i = 1; i < n; i++) {
+        if (s[i] == '0' && s[i - 1] == '0') {
+            cout << -1 << "\n";
+            return;
         }
     }
-    return 1;
+    long long lo = 1, hi = 2LL * n + 5, ans = -1;
+    while (lo <= hi) {
+        long long mid = lo + (hi - lo) / 2;
+        if (chk(mid)) {
+            ans = mid;
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    cout << ans << "\n";
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
-    if (!(cin >> t)) return 0;
-    while (t--) {
-        int n;
-        string s;
-        cin >> n >> s;
-        int a = 1, b = n, z = -1;
-        while (a <= b) {
-            int m = (a + b) / 2;
-            if (f(m, s, n)) {
-                z = m;
-                b = m - 1;
-            } else {
-                a = m + 1;
-            }
+    if (cin >> t) {
+        while (t--) {
+            run();
         }
-        cout << z << "\n";
     }
     return 0;
 }
