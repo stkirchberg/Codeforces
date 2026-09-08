a83e3ee5d45556692f919aee247bcb4227f58ef6
stkirchberg <s.t.kirchberg@web.de>
2026-09-08 17:02:57 +0200

    still wriong

--- E194B.cpp
+++ E194B.cpp
@@ -4,22 +4,11 @@ using namespace std;
 void solve() {
     long long x, y, k;
     cin >> x >> y >> k;
-    if (x > y) {
-        long long s = y * k + k * (k - 1) / 2;
-        cout << s << "\n";
-        return;
-    }
-    long long d = y - x;
     long long s = 0;
-    long long m = d - x;
-    if (m >= k) {
-        m = k - 1;
-    }
-    for (long long i = 0; i <= m; i++) {
-        s += d % (x + 1);
-    }
-    if (k > m + 1) {
-        s += (k - (m + 1)) * d;
+    for (long long i = 0; i < k; i++) {
+        long long a = x + i;
+        long long b = y + i;
+        s += b % a;
     }
     cout << s << "\n";
 }
@@ -27,10 +16,10 @@ void solve() {
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
+    long long t;
     cin >> t;
     while (t--) {
         solve();
     }
     return 0;
-} 
+}
